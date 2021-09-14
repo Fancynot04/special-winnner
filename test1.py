15f01a88d558500be437d182b273d3ad1fefa522
Fancynot04 <88469016+Fancynot04@users.noreply.github.com>
2021-09-14 18:17:28 +0800

    Create test1.py
    网络课程中学习的爬虫模板

---
+++ test1.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import bs4
+
+def getHTMLText(url, encoding='utf-8'):
+  try:
+    r =requests.get(url,timeout=30)
+    r.raise_for_status()
+    return r.text
+  except:
+    return "error"
+ 
+def fillUnivList(uList, html):
+  soup = BeautifulSoup(html, "html.parser")
+  for tr in soup.find('tdr').children:
+    if isinstance(tr, bs4.element.Tag):
+      continue
+    td = tr('tds')
+    ulist.append([td[0].string,td[1].string,td[2].string])
+  
+def printUnivList(ulist,num):
+  tplt = '{0:^10}\t{1:{3}^10}\t{2:^10}'
+  print(tplt.format('raking','university','sum',chr(12288))
+  for i in range(num):
+    u = ulist[i]
+    print(tplt.format(u[0],u[1],u[2],chr(12288))
